a911cd7b4ebb5e76d5faf98c5f7e552040a803b8
tricialeach <tleach@gmail.com>
2017-07-04 14:28:07 -0700

    Added validations for email address and linked them to form submit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# TO DO:
+# link up the small functions to the email validations
+
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -15,13 +18,17 @@ user_signup_form = """
         </label>
         <p class="error">{username_error}</p>
         <label>Password
-            <input name="password" type="text" value='{password}' />
+            <input name="password" type="password" value='{password}' />
         </label>
         <p class="error">{password_error}</p>
         <label>Password Validate
-            <input name="password_validate" type="text" value='{password_validate}' />
+            <input name="password_validate" type="password" value='{password_validate}' />
         </label>
         <p class="error">{password_validate_error}</p>
+        <label>E-mail (optional)
+            <input name="email" type="text" value='{email}' />
+        </label>
+        <p class="error">{email_error}</p>
         <input type="submit" value="Submit" />
     </form>
     """
@@ -51,13 +58,25 @@ def has_space(x):
         return False
 
 def email_at_symbol(x):
-    if x.count('@') == 1:
+    if x.count('@') >= 1:
+        return True
+    else:
+        return False
+
+def email_at_symbol_more_than_one(x):
+    if x.count('@') <= 1:
         return True
     else:
         return False
 
 def email_period(x):
-    if x.count('.') == 1:
+    if x.count('.') >= 1:
+        return True
+    else:
+        return False
+
+def email_period_more_than_one(x):
+    if x.count('.') <= 1:
         return True
     else:
         return False
@@ -68,10 +87,12 @@ def user_signup_complete():
     username = request.form['username']
     password = request.form['password']
     password_validate = request.form['password_validate']
+    email = request.form['email']
 
     username_error = ""
     password_error = ""
     password_validate_error = ""
+    email_error = ""
 
     # THIS IS THE USERNAME VALIDATION
 
@@ -105,11 +126,28 @@ def user_signup_complete():
         if password_validate != password:
             password_validate_error = "Passwords must match"
 
+    # THIS IS THE EMAIL VALIDATION
+
+    if not char_length(email):
+        email_error = "Email must be between 3 and 20 characters"
+    elif not email_at_symbol(email):
+        email_error = "Email must contain the @ symbol"
+    elif not email_at_symbol_more_than_one(email):
+        email_error = "Email must contain only one @ symbol"
+    elif not email_period(email):
+        email_error = "Email must contain a ."
+    elif not email_period_more_than_one(email):
+        email_error = "Email must contain only one ."
+    else:
+        if " " in email:
+            email_error = "Email must not contain spaces"
+
     # THIS IS THE FINAL RESULT
 
-    if not username_error and not password_error and not password_validate_error:
+    if not username_error and not password_error and not password_validate_error and not email_error:
         return 'Success'
+        #return '<h1>Welcome, ' + username + '</h1>'
     else:
-        return user_signup_form.format(username_error=username_error, username=username, password_error=password_error, password=password, password_validate_error=password_validate_error, password_validate=password_validate)
+        return user_signup_form.format(username_error=username_error, username=username, password_error=password_error, password=password, password_validate_error=password_validate_error, password_validate=password_validate, email_error=email_error, email=email)
 
 app.run()
