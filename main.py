a87f13156fcd59ec1c53599104f0bc9540f919a6
tricialeach <tleach@gmail.com>
2017-07-05 17:34:48 -0700

    Initialized jinja and linked to template folder at top of python file

--- main.py
+++ main.py
@@ -3,6 +3,13 @@
 
 from flask import Flask, request, redirect
 
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
